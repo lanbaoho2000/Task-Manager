b450ad412944f1c7ee68307b2be8fa232ebce81a
blanc252023 <h.baolan20022@gmail.com>
2022-08-01 10:32:47 +0700

    Catching Error

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/404.js");
+const errorHandler = require("./middleware/error.js");
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandler);
+const port = process.env.PORT || 3000;
 
-const port = 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
